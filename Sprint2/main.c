ceedb712ff6807f2259e5987c01e228469ff9e22
Zero97M <mrane2010@gmail.com>
2018-10-01 11:25:42 -0600

    Added user string input functionality

--- Sprint2/main.c
+++ Sprint2/main.c
@@ -8,49 +8,50 @@ int main(int argc, char **argv)
     printf("Welcome User\n");
     int (*func_arr[])(int) = {help, allo, free_mem, write, display};
     int **memptr;
+    char user_function_input[10];	//Defined a variable for user input
     func_arr[0](1);
-    char help = 'h';
-    char exit = 'e';
-    char allo = 'a';
-    char free = 'f';
-    char write = 'w';
-    char display = 'd';    
+    char help[4] = "help";
+    char exit[4] = "exit";
+    char allo[4] = "allo";
+    char free[4] = "free";
+    char write[5] = "write";
+    char display[7] = "display";    
    // scanf("%c",&ftbe);    
 while(1)
 {
-Label:      scanf("%c",&ftbe);
-	    if(ftbe==help)
+Label:      scanf("%s", user_function_input);
+	    if(strcmp(user_function_input, help))
 		{
 			printf("\n");
 			func_arr[0](1);
 			ftbe=takeinput();
 		}
 
-            else if(ftbe==allo)
+            else if(strcmp(user_function_input, allo))
                 {
                         **memptr=func_arr[1](1);
 			ftbe=takeinput();
                 }
 
-	    else if(ftbe == free)
+	    else if(strcmp(user_function_input, free))
 		{
 			func_arr[2](**memptr);
 			ftbe=takeinput();
 		}
 
-	    else if(ftbe == exit)
+	    else if(strcmp(user_function_input, exit))
 		{
 			printf("Terminating.......\n");
 			break;
 		}
 	    
-            else if(ftbe == write)
+            else if(strcmp(user_function_input, write))
 		{
 			func_arr[3](1);
 			takeinput();
 		}
 
-	    else if(ftbe == display)
+	    else if(strcmp(user_function_input, display))
   		{
 			func_arr[4](1);
 			takeinput();
