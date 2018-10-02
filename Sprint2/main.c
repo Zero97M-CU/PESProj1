a0112415a767e4d94b83073698b3580f6aff6916
Zero97M-CU <mohit.rane@colorado.edu>
2018-10-02 12:34:36 -0600

    Fixed error - Appropriate function was not being called after taking user input. And enhanced code readability.

--- Sprint2/main.c
+++ Sprint2/main.c
@@ -5,56 +5,74 @@ char ftbe;
 
 int main(int argc, char **argv)
 {       
+    //Greetings
     printf("Welcome User\n");
+    
+    //Declaring function array pointers
     int (*func_arr[])(int) = {help, allo, free_mem, write, display};
+    
     int **memptr;
-    char user_function_input[10];	//Defined a variable for user input
+    
+    //Defined a variable for user input
+    char user_func_in[10];
+    
+    //Calling help function
     func_arr[0](1);
-    char help[4] = "help";
-    char exit[4] = "exit";
-    char allo[4] = "allo";
-    char free[4] = "free";
-    char write[5] = "write";
-    char display[7] = "display";    
-   // scanf("%c",&ftbe);    
+
+    //Declaring strings to compare with user input and call correct function
+    char help[5] = "help";
+    char exit[5] = "exit";
+    char allo[5] = "allo";
+    char free[5] = "free";
+    char write[6] = "write";
+    char display[8] = "display";
+    
+    //Declaring an integer to store the string compare value obtained 
+    //from user input and pre-defined functions
+    //int func_cmp_value;
+   
+    // scanf("%c",&ftbe);    
+
 while(1)
 {
-Label:      scanf("%s", user_function_input);
-	    if(strcmp(user_function_input, help))
+Label:      //Scans user input
+	    scanf("%s", user_func_in);
+	    
+	    if(strcmp(user_func_in, help) == 0)
 		{
 			printf("\n");
 			func_arr[0](1);
 			ftbe=takeinput();
 		}
 
-            else if(strcmp(user_function_input, allo))
+            else if(strcmp(user_func_in, allo) == 0)
                 {
                         **memptr=func_arr[1](1);
 			ftbe=takeinput();
                 }
 
-	    else if(strcmp(user_function_input, free))
+	    else if(strcmp(user_func_in, free) == 0)
 		{
 			func_arr[2](**memptr);
 			ftbe=takeinput();
 		}
 
-	    else if(strcmp(user_function_input, exit))
-		{
-			printf("Terminating.......\n");
-			break;
-		}
-	    
-            else if(strcmp(user_function_input, write))
+	    else if(strcmp(user_func_in, write) == 0)
 		{
 			func_arr[3](1);
 			takeinput();
 		}
 
-	    else if(strcmp(user_function_input, display))
+	    else if(strcmp(user_func_in, display) == 0)
   		{
 			func_arr[4](1);
 			takeinput();
+		}
+
+	    else if(strcmp(user_func_in, exit) == 0)
+		{
+			printf("Terminating.......\n");
+			break;
 		}
 
 	    else
